b3284e965deebbf186c82f1e170ab2b4acc03f3c
Yjongbeom <139727424+Yjongbeom@users.noreply.github.com>
2025-08-12 16:30:38 +0900

    Update OpenAiClient.java

--- src/main/java/contest/mobicom_contest/contract/client/OpenAiClient.java
+++ src/main/java/contest/mobicom_contest/contract/client/OpenAiClient.java
@@ -2,6 +2,7 @@ package contest.mobicom_contest.contract.client;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
